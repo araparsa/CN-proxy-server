5953c1841fcc20f44c10ab5e5dce35c5ac3dd23f
arman <58548100+araparsa@users.noreply.github.com>
2019-12-08 22:38:47 +0330

    Add files via upload

---
+++ code/cacheHandler.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: arman
+# @Date:   2019-12-08 22:25:52
+# @Last Modified by:   arman
+# @Last Modified time: 2019-12-08 22:26:33
+
+class CacheHandler():
+	"""docstring for CacheHandler"""
+	def __init__(self, arg):
