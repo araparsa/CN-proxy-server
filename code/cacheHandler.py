5757a72d85a1aadfb6634ad898a47a383be55bb2
arapa98 <a.rparsayei1998@yahoo.com>
2019-12-09 13:54:25 +0330

    refactor

--- code/cacheHandler.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: arman
-# @Date:   2019-12-08 22:25:52
-# @Last Modified by:   arman
-# @Last Modified time: 2019-12-08 22:26:33
-
-class CacheHandler():
-	"""docstring for CacheHandler"""
-	def __init__(self, arg):
